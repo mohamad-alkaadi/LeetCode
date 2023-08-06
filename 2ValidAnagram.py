6222256a4a62881727c9490ec2804eb59880b9d5
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-06 15:43:59 +0300

    learned solution

--- 2ValidAnagram.py
+++ 2ValidAnagram.py
@@ -1,3 +1,4 @@
+# my solution
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         string1 = list(s)
@@ -10,3 +11,16 @@ class Solution:
                 return False
             string1.remove(string2[i])
         return True
+
+
+# learned solution 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#            return 0
+#         string1, string2 = {}, {}
+
+#         for i in range(len(s)):
+#             string1[s[i]] = 1+ string1.get(s[i], 0)
+#             string2[t[i]] = 1+ string2.get(t[i], 0)
+#         return string1 == string2
