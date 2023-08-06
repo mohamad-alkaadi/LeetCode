6254a0e4e2220d5e944626e809ff50bccacc336c
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-06 15:27:11 +0300

    slve secound problem

--- 2ValidAnagram.py
+++ 2ValidAnagram.py
@@ -1,31 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        z = 0
-        for x in s:
-            for y in t:
-                if y in x:
-                    z = 1
-                else:
-                    z = 0
-        if z == 0:
+        string1 = list(s)
+        string2 = list(t)
+        x = len(string1)
+        if len(string1) != len(string2):
             return False
-        elif z == 1:
-            return True
-
-
-    #     class Solution:
-    # def isAnagram(self, s: str, t: str) -> bool:
-    #     exSet = set()
-    #     z = 0
-        
-    #     for x in s:
-    #         exSet.add(x)
-    #         for y in t:
-    #             if y in exSet:
-    #                 z = 1
-    #             else:
-    #                 z = 0
-    #     if z == 0:
-    #         return False
-    #     elif z == 1:
-    #         return True
+        for i in range(x):
+            if string2[i] not in string1:
+                return False
+            string1.remove(string2[i])
+        return True
